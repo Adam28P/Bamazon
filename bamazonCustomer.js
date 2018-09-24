61ba6723ac42dc18e7f6a5e37731dfb032807d61
Adam P <adamparalovos@hotmail.com>
2018-09-23 23:03:44 -0400

    Modify products table contents

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,34 @@
 //require mysql and inquirer
 var mysql = require('mysql');
-var inquirer = require('inquirer');
+// var inquirer = require('inquirer');
+
+//create connection to database
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "bamazon"
+})
+
+connection.connect(function(err){
+    console.log("Connected as id: " + connection.threadId);
+    start();
+})
+
+function start() {
+    //prints the items for sale and their details
+    connection.query('SELECT * FROM products', function (err, res) {
+        if (err) throw err;
+
+        console.log('~~~~~~~~~~~~~~~ Welcome to Bamazon ~~~~~~~~~~~~~~~')
+        console.log('----------------------------------------------------------------------------------------------------')
+
+        for (var i = 0; i < res.length; i++) {
+            console.log("ID: " + res[i].item_id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "QTY: " + res[i].stock_quantity);
+            console.log('--------------------------------------------------------------------------------------------------')
+        }
+        console.log(' ');
+
+    });
+}
--- schema.sql
+++ schema.sql
@@ -12,14 +12,14 @@ stock_quantity INT NOT NULL DEFAULT '0'
 
 INSERT INTO products(product_name,department_name,price,stock_quantity)
 VALUES ("Overwatch","Electronics",79.99,500),
-    ("Nintendo Switch","Electronics",400.00,250),
-    ("Apples","Produce",10.00,50),
-    ("iPhone","Electronics",200.00,100),
-    ("Dell Laptop","Electronics",1500.00,35),
-    ("Sony Television","Electronics",350.00,28),
-    ("Bananas","Produce",5.00,25),
-    ("The Simpsons Comic Book","Entertainment",25.50,60),
-    ("American Horror Story Funko Pop","Entertainment",12.00,10),
+    ("Nintendo Switch","Electronics",399.99,250),
+    ("Apples","Produce",9.99,50),
+    ("iPhone","Electronics",199.99,100),
+    ("Dell Laptop","Electronics",1499.99,35),
+    ("Sony Television","Electronics",349.99,28),
+    ("Bananas","Produce",4.99,25),
+    ("The Simpsons Comic Book","Entertainment",24.99,60),
+    ("American Horror Story Funko Pop","Entertainment",12.99,10),
     ("Watermelon","Produce",19.99,75);
 
 SELECT * FROM products;
