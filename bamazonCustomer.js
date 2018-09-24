2c0de4a34bb443796c55528521d2f00505e3183b
Adam P <adamparalovos@hotmail.com>
2018-09-23 23:56:21 -0400

    Prompt user to buy product and perform calculations

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 //require mysql and inquirer
 var mysql = require('mysql');
-// var inquirer = require('inquirer');
+var inquirer = require('inquirer');
 
 //create connection to database
 var connection = mysql.createConnection({
@@ -11,16 +11,17 @@ var connection = mysql.createConnection({
     database: "bamazon"
 })
 
-connection.connect(function(err){
+connection.connect(function (err) {
     console.log("Connected as id: " + connection.threadId);
     start();
 })
 
-function start() {
+var start = function () {
     //prints the items for sale and their details
     connection.query('SELECT * FROM products', function (err, res) {
         if (err) throw err;
 
+        console.log(' ');
         console.log('~~~~~~~~~~~~~~~ Welcome to Bamazon ~~~~~~~~~~~~~~~')
         console.log('----------------------------------------------------------------------------------------------------')
 
@@ -30,5 +31,56 @@ function start() {
         }
         console.log(' ');
 
+        inquirer.prompt([{
+            name: "idToPurchase",
+            type: "input",
+            message: "What is the ID of the product you would like to buy?"
+        }, {
+            name: "quantityToBuy",
+            type: "input",
+            message: "How many units of the product would you like to buy?"
+        }]).then(function (answer) {
+            var whatToBuy = (answer.idToPurchase) - 1;
+            var howMuchToBuy = parseInt(answer.quantityToBuy);
+            var grandTotal = parseFloat(((res[whatToBuy].price) * howMuchToBuy).toFixed(2));
+
+            //check if quantity is sufficient
+            if (res[whatToBuy].stock_quantity >= howMuchToBuy) {
+                //after purchase, updates quantity in Products
+                connection.query("UPDATE products SET ? WHERE ?", [{
+                        stock_quantity: (res[whatToBuy].stock_quantity - howMuchToBuy)
+                    },
+                    {
+                        item_id: answer.idToPurchase
+                    }
+                ], function (err, result) {
+                    if (err) throw err;
+                    console.log(' ');
+                    console.log("Success! Your total is $" + grandTotal.toFixed(2) + ". Your item(s) will be shipped to you in 3-5 business days.");
+                    console.log(' ');
+                });
+            } else {
+                console.log(' ');
+                console.log("Sorry, there's not enough in stock!");
+                console.log(' ');
+            }
+            restart();
+        });
+    });
+}
+
+//asks if they would like to purchase another item
+function restart() {
+    inquirer.prompt([{
+        name: "restart",
+        type: "confirm",
+        message: "Would you like to purchase another item?"
+    }]).then(function (answer) {
+        if (answer.restart) {
+            start();
+        } else {
+            console.log(' ');
+            console.log("Thank you, come again!");
+        }
     });
 }
