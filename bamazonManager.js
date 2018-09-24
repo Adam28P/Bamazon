dc3cff0529481350ce8a38eac139e677265a7613
Adam P <adamparalovos@hotmail.com>
2018-09-24 00:44:35 -0400

    function to update inventory in store

--- bamazonManager.js
+++ bamazonManager.js
@@ -59,3 +59,78 @@ function viewProducts() {
         start();
     });
 }
+
+// function to view low inventory products
+function viewLowInventory() {
+    connection.query('SELECT * FROM products WHERE stock_quantity < 5', function (err, res) {
+        if (err) throw err;
+
+        if (res.length > 0) {
+            console.log(' ');
+            console.log('~~~~~~~~~~~~~~~ Products with lower than 5 stock ~~~~~~~~~~~~~~~')
+            console.log('----------------------------------------------------------------------------------------------------')
+
+            for (var i = 0; i < res.length; i++) {
+                console.log("ID: " + res[i].item_id + " | " + "Product: " + res[i].product_name + " | " + "Department: " + res[i].department_name + " | " + "Price: " + res[i].price + " | " + "QTY: " + res[i].stock_quantity);
+                console.log('--------------------------------------------------------------------------------------------------')
+            }
+            console.log(' ');
+            start();
+        } else {
+            console.log(' ');
+            console.log('There are currently no products with an inventory lower than 5.');
+            console.log(' ');
+            start();
+        }
+    });
+}
+
+// function to add more of a quantity of a specific product
+function addToInventory() {
+    connection.query('SELECT * FROM products', function (err, res) {
+        if (err) throw err;
+
+        var itemArray = [];
+
+        //pushes each item into an itemArray
+        for (var i = 0; i < res.length; i++) {
+            itemArray.push(res[i].product_name);
+        }
+
+        inquirer.prompt([{
+            name: "addInventoryToProduct",
+            type: "list",
+            choices: itemArray,
+            message: "Which item would you like to add inventory to?"
+        }, {
+            name: "stockToAdd",
+            type: "input",
+            message: "How much stock would you like to add to this item?"
+        }]).then(function (answer) {
+            var currentQuantity;
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].product_name === answer.addInventoryToProduct) {
+                    currentQuantity = res[i].stock_quantity;
+                }
+            }
+            connection.query('UPDATE products SET ? WHERE ?', [{
+                    stock_quantity: currentQuantity + parseInt(answer.stockToAdd)
+                },
+                {
+                    product_name: answer.addInventoryToProduct
+                }
+            ], function (err, res) {
+                if (err) throw err;
+                console.log(' ');
+                console.log('The quantity was updated. You have added ' + answer.stockToAdd + ' stock to ' + answer.addInventoryToProduct + '.');
+                console.log(' ');
+                start();
+            });
+        });
+    });
+}
+
+// function to allow manager to add a completely new product to store
+function addNewProduct() {
+
+}
